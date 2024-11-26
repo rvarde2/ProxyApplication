b2b49a437f736c8e1449bcfcf0abb68ebd8086d9
Rohan Vardekar <rvarde2@uic.edu>
2024-11-26 08:23:37 -0700

    Minor Updates

--- pvapy_tester.sh
+++ pvapy_tester.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 # Check if two arguments are provided
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <IP_ADDRESS:PORT> <producer/consumer>"
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <producer ip> <port> <producer/consumer>"
     exit 1
 fi
 
-# Assign arguments to variables
-export EPICS_PVA_NAME_SERVER=$1
-MODE=$2
+MODE=$3
 CHANNEL_NAME=pvapy:image
+FRAME_RATE=1
+DURATION=10
+REPORT_RATE=1
+export EPICS_PVA_BROADCAST_PORT=1000
 
 # Check the mode and display appropriate message
 if [ "$MODE" == "producer" ]; then
-    pvapy-ad-sim-server -cn $CHANNEL_NAME -nx 128 -ny 128 -dt uint8 -rt 60 -fps 10000 -rp 10000
+    export EPICS_PVA_SERVER_PORT=$2
+    pvapy-ad-sim-server -cn $CHANNEL_NAME -nx 128 -ny 128 -dt uint8 -rt $DURATION -fps $FRAME_RATE -rp $REPORT_RATE
 elif [ "$MODE" == "consumer" ]; then
+    export EPICS_PVA_NAME_SERVER=$1:$2
     pvapy-hpc-consumer \
     --input-channel $CHANNEL_NAME \
     --control-channel consumer:*:control \
